3f70a07163dd575d788f178f425bb8a9de0c863c
alvinh16 <alvinho@yandex.com>
2024-05-09 16:27:45 +0800

    initial commit

---
+++ PandasTest.py
@@ -0,0 +1,6 @@
+#!/home/alvinh/.virtualenvs/pyscratchspace/bin/python3
+
+import pandas as pd
+
+df = pd.read_csv("manjarotest.csv")
+print (df)
---
+++ timpy101ex10.py
@@ -0,0 +1,11 @@
+#!/home/alvinh/.virtualenvs/pyscratchspace/bin/python3
+def tough (lion, panther, *tiger):
+
+    print ("the value of lion is : ", lion)
+    print ("the value of panther is : ", panther)
+    print ("the value of tiger is : ", tiger)
+    print ("the 4th element in the tuple is, ", tiger[3])
+    print ()
+    pass
+
+tough (1, 3, 7, 5, 14, 11, 19, 17, 23, 4)
